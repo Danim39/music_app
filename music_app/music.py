ffeee060abdd64e87ba2ce3954b569103ada899f
Danim <foortnite48@gmail.com>
2024-03-22 08:25:08 -0700

    add 2 iranian music

--- music_app/music.py
+++ music_app/music.py
@@ -65,7 +65,7 @@ class Form(QWidget):
                             'i love you.mp3' , 'Toomaj - Tifus.mp3' , 'Pastlives - sapientdream.mp3',
                                 'Cant Get You out Of My Head.mp3' , 'i dont like it, youre not the same.mp3' , 
                                     'Mockingbird.mp3' , 'dance with me.mp3' , 'Space Song.mp3' , 'Six Feet Under.mp3',
-                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3' ]
+                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3','Age Ye Rooz - Remix.mp3' , 'Erfan, Gdaal, Imanemun - Be Yade - Remix.mp3']
             #Line Edit (Enter text for play music)       
         if self.lineEdit.text() == "kerkere":
             pygame.init()
@@ -176,6 +176,16 @@ class Form(QWidget):
         elif self.lineEdit.text()=="ending":
             pygame.init()
             pygame.mixer.music.load(self.music_list[21])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="age ye rooz erfan":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[22])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="be yade":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[23])
             pygame.mixer.music.play()
 
         # show message if the user doesn't type any music
