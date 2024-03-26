50795e47b1a8001e95269220aa049ad223b8de88
Danim <158760301+Danim39@users.noreply.github.com>
2024-03-27 02:09:47 +0330

    Add 2 music

--- music_app/music.py
+++ music_app/music.py
@@ -64,7 +64,7 @@ class Form(QWidget):
                             'i love you.mp3' , 'Toomaj - Tifus.mp3' , 'Pastlives - sapientdream.mp3',
                                 'Cant Get You out Of My Head.mp3' , 'i dont like it, youre not the same.mp3' , 
                                     'Mockingbird.mp3' , 'dance with me.mp3' , 'Space Song.mp3' , 'Six Feet Under.mp3',
-                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3','Age Ye Rooz - Remix.mp3' , 'Erfan, Gdaal, Imanemun - Be Yade - Remix.mp3' , 'Cheri Cheri Lady.mp3' , 'Billie Eilish - TV.mp3' , 'Hope.mp3' , 'Fairytale.mp3' , 'Shootout.mp3' , 'Powfu, beabadoobee - death bed (coffee for your head).mp3']
+                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3','Age Ye Rooz - Remix.mp3' , 'Erfan, Gdaal, Imanemun - Be Yade - Remix.mp3' , 'Cheri Cheri Lady.mp3' , 'Billie Eilish - TV.mp3' , 'Hope.mp3' , 'Fairytale.mp3' , 'Shootout.mp3' , 'Powfu, beabadoobee - death bed (coffee for your head).mp3', 'ASADI, Erfan, Xye - Hide & Seek.mp3' , 'AirplaneMode.mp3']
             #Line Edit (Enter text for play music)       
         if self.lineEdit.text() == "kerkere":
             pygame.init()
@@ -216,7 +216,17 @@ class Form(QWidget):
             pygame.init()
             pygame.mixer.music.load(self.music_list[29])
             pygame.mixer.music.play()
-        
+
+        elif self.lineEdit.text()=="hide and seek":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[30])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="airplanemode":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[31])
+            pygame.mixer.music.play()
+
         # show message if the user doesn't type any music
         elif self.lineEdit.text()=="":
             message=QMessageBox()
