41701b7f55ee54cd56c9a2f8f4e9e14ad1478842
Danim <158760301+Danim39@users.noreply.github.com>
2024-04-03 19:14:02 +0330

    Add 2 music

--- README.md
+++ README.md
@@ -27,4 +27,4 @@ Music:
 
   Yas - LaL , NF - The Search , Rio Romeo - Nothing's New , SpoMehrad Hidden Sepehr Khalse - Khodafez , Summertime Sadness
 
-  AURORA - Runaway
+  AURORA - Runaway , Vengeance , Bawaasir ,   Lil Peep - Star Shopping , Miley Cyrus - Flowers
--- music_app/music.py
+++ music_app/music.py
@@ -64,7 +64,7 @@ class Form(QWidget):
                             'i love you.mp3' , 'Toomaj - Tifus.mp3' , 'Pastlives - sapientdream.mp3',
                                 'Cant Get You out Of My Head.mp3' , 'i dont like it, youre not the same.mp3' , 
                                     'Mockingbird.mp3' , 'dance with me.mp3' , 'Space Song.mp3' , 'Six Feet Under.mp3',
-                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3','Age Ye Rooz - Remix.mp3' , 'Erfan, Gdaal, Imanemun - Be Yade - Remix.mp3' , 'Cheri Cheri Lady.mp3' , 'Billie Eilish - TV.mp3' , 'Hope.mp3' , 'Fairytale.mp3' , 'Shootout.mp3' , 'Powfu, beabadoobee - death bed (coffee for your head).mp3', 'ASADI, Erfan, Xye - Hide & Seek.mp3' , 'AirplaneMode.mp3' , 'tommymuzzic_another_love_x_summertime_sadness.mp3' , 'Billie Eilish - lovely (with Khalid).mp3' , 'Arta & Koorosh - Zendegi Hamine.mp3' , 'Go Little Rockstar.mp3' , 'Yas - BEEM  بیم.mp3' , 'Yas - Sarkoob.mp3' ,'Yas - LaL.mp3' , 'NF - The Search.mp3' , 'Rio Romeo - Nothings New.mp3' , 'SpoMehrad Hidden Sepehr Khalse - Khodafez.mp3' , 'AURORA - Runaway.mp3' , 'Summertime Sadness.mp3']
+                                        'Roozhaaye Khoobe Koodaki.mp3' , 'Losing Interest.mp3' , 'Freaks but Slowed Muffled Echo.mp3' , 'Its Not So Bad.mp3' , 'Reza Pishro - Ghabrestoune Hip Hop.mp3' , 'Palangi.mp3', 'Baby Bebin Baroone.mp3' , 'Cigare Soorati.mp3' , 'WASTE - Slowed Version.mp3' , 'Ending.mp3','Age Ye Rooz - Remix.mp3' , 'Erfan, Gdaal, Imanemun - Be Yade - Remix.mp3' , 'Cheri Cheri Lady.mp3' , 'Billie Eilish - TV.mp3' , 'Hope.mp3' , 'Fairytale.mp3' , 'Shootout.mp3' , 'Powfu, beabadoobee - death bed (coffee for your head).mp3', 'ASADI, Erfan, Xye - Hide & Seek.mp3' , 'AirplaneMode.mp3' , 'tommymuzzic_another_love_x_summertime_sadness.mp3' , 'Billie Eilish - lovely (with Khalid).mp3' , 'Arta & Koorosh - Zendegi Hamine.mp3' , 'Go Little Rockstar.mp3' , 'Yas - BEEM  بیم.mp3' , 'Yas - Sarkoob.mp3' ,'Yas - LaL.mp3' , 'NF - The Search.mp3' , 'Rio Romeo - Nothings New.mp3' , 'SpoMehrad Hidden Sepehr Khalse - Khodafez.mp3' , 'AURORA - Runaway.mp3' , 'Summertime Sadn ,ess.mp3' , 'Bawaasir.mp3' , 'Miley Cyrus - Flowers.mp3' , 'LUCKY LOVE - MASCULINITY.mp3' , 'Lil Peep - Star Shopping.mp3' , 'Vengeance.mp3']
             #Line Edit (Enter text for play music)       
         if self.lineEdit.text() == "kerkere":
             pygame.init()
@@ -286,6 +286,31 @@ class Form(QWidget):
         elif self.lineEdit.text()=="summer sadness":
             pygame.init()
             pygame.mixer.music.load(self.music_list[43])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="bewaasir":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[44])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="flower":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[45])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="lucky love":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[46])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="star shopping":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[47])
+            pygame.mixer.music.play()
+
+        elif self.lineEdit.text()=="vengeance":
+            pygame.init()
+            pygame.mixer.music.load(self.music_list[48])
             pygame.mixer.music.play()
 
         # show message if the user doesn't type any music
